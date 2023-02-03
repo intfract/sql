43ef30f88c1208c82fabed8e5d17413ee48df667
Int Fract <110207136+intfract@users.noreply.github.com>
2023-02-04 11:20:25 +1300

    added easy queries

--- main.sql
+++ main.sql
@@ -1,1 +1,11 @@
+-- Select all the columns from table_name 
+SELECT * FROM table_name;
 
+-- Select all the cells in the column_name column from table_name 
+SELECT column_name FROM table_name;
+
+-- Select the cells in the column_name column from table_name that have a number_column value greater than 0 
+SELECT column_name FROM table_name WHERE number_column > 0;
+
+-- Display the number of cells in the column_name column from table_name that have a number_column value greater than 0 
+SELECT COUNT(column_name) FROM table_name WHERE number_column > 0;
